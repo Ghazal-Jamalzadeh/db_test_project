41b4bf4475a06ae896bbc2bcee0fc094d2d089ef
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-08-25 23:07:36 +0430

    removed useless classes and methods

--- lib/utils/date_time_manager.dart
+++
@@ -1,5 +0,0 @@
-class DateTimeManager {
-  static DateTime generateDateTimeFromPickerStuff(DateTime date, String time) {
-    return DateTime.parse(date.toString().replaceRange(11, 16, time));
-  }
-}
