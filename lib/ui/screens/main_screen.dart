3b861876c6b78e1be04ffaf5d5e185f60e7bb98a
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-08-25 18:56:08 +0430

    Complete UI

--- lib/ui/screens/main_screen.dart
+++ lib/ui/screens/main_screen.dart
@@ -1,11 +1,70 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class MainScreen extends StatelessWidget {
-   MainScreen({Key? key}) : super(key: key);
+  MainScreen({Key? key}) : super(key: key);
+
+  TextEditingController firstNameController = TextEditingController() ;
+  TextEditingController lastNameController = TextEditingController() ;
+  TextEditingController dateOfBirthController = TextEditingController() ;
+  TextEditingController phoneNumberController = TextEditingController() ;
+  TextEditingController emailController = TextEditingController() ;
+  TextEditingController bankAccountController = TextEditingController() ;
 
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.red,);
+
+    return Scaffold(body: SafeArea(
+      child: Container(
+        height: double.infinity,
+        child: Center(
+          child: SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                children: [
+                  TextField(
+                      controller: firstNameController,
+                      decoration: const InputDecoration(labelText: 'FirstName')
+                  ) ,
+                  TextField(
+                      controller: lastNameController,
+                      decoration: const InputDecoration(labelText: 'LastName')
+                  ) ,
+                  TextField(
+                      controller: dateOfBirthController,
+                      decoration: const InputDecoration(labelText: 'DateOfBirth')
+                  ) ,
+                  TextField(
+                      controller: phoneNumberController,
+                      decoration: const InputDecoration(labelText: 'PhoneNumber')
+                  ) ,
+                  TextField(
+                      controller: emailController,
+                      decoration: const InputDecoration(labelText: 'Email')
+                  ) ,
+                  TextField(
+                      controller: bankAccountController,
+                      decoration: const InputDecoration(labelText: 'BankAccount')
+                  ) ,
+                  const SizedBox(height: 32,) ,
+                  SizedBox(
+                    width: double.infinity,
+                    child: MaterialButton(
+                      color: Colors.green,
+                      textColor: Colors.white,
+                      child: const Text('SUBMIT'),
+                        onPressed: (){
+
+                    }),
+                  ) ,
+
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    ));
   }
 }
